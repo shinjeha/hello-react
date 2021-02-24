5a5b33ea8ff1d20c176b1ae8c65fed76844e042f
shinjeha1993 <shinjeha1993@gmail.com>
2021-02-25 01:13:38 +0900

    eventHandling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 //import MyComponent from './MyComponent';
 //import Counter from './Counter';
-import Say from './Say';
+//import Say from './Say';
+import EventPractice from './EventPractice';
 
 const App = () => {
   //return <MyComponent name="123" favoriteNumber={2}>리액트</MyComponent>;
   //return <Counter />
-  return <Say />
+  //return <Say />
+  return <EventPractice />;
 };
 
 export default App;
